89b7d0f208c8a4cc4bd38e39ebcae44f00686016
WORKSTATION06/SDP/BRAC <WORKSTATION06@brac.net>
2025-07-17 03:11:08 +0600

    jwt implimented in all user

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -10,10 +10,12 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+const axiosPublic = useAxiosPublic();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -45,6 +47,18 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      if (currentUser) {
+        //get toekn and store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        //remove token(if store in the client side: local storage, caching, in memory)
+        localStorage.removeItem("access-token");
+      }
       setLoading(false);
     });
     return () => {
